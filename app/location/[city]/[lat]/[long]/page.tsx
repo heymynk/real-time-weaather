e80b0066ada49a042e8f3b669396440312be6bf3
Hey-Mynk <mail.contactmayank@gmail.com>
2024-09-06 13:23:20 +0530

    commit

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,6 +1,7 @@
 import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import StatCard from "@/components/StatCard";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
-
 
 type Props = {
   params: {
@@ -19,11 +20,12 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
       current_weather: "true",
       longitude: long,
       latitude: lat,
+      city: city,
       timezone: "GMT",
     },
   });
 
-  const results: Root = data.myQuery
+  const results: Root = data.myQuery;
   console.log(results);
 
   return (
@@ -33,11 +35,23 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
       <div>
         <div className="p-5">
           <div className="pb-5">
-            <h2 className="text-xl font-bold">Today's Overview</h2>
-            <p className="text-sm text-gray-500">Last Updated at: {" "}
-              {new Date(results.current_weather.time).toLocaleString()}
-              ({results.timezone})
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-500">
+              Last Updated at:{" "}
+              {new Date(results.current_weather.time).toLocaleString()}(
+              {results.timezone})
             </p>
+          </div>
+          <div>
+            {/* CalloutCard */}
+            <CalloutCard message="This is where GPT- 3.5 Summary will go!" />
+          </div>
+          <div>
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${results.daily.temperature_2m_max[0].toFixed(1)}˚C`}
+              color="blue"
+            />
           </div>
         </div>
       </div>
---
+++ components/CalloutCard.tsx
@@ -0,0 +1,37 @@
+import {
+    CheckCircleIcon,
+    ExclamationTriangleIcon,
+  } from "@heroicons/react/24/solid";
+  
+  type Props = {
+    message: string;
+    warning?: boolean;
+  };
+  
+  function CalloutCard({ message, warning }: Props) {
+    return (
+      <div
+        className={`mt-4 p-4 rounded-lg shadow-md transition-all duration-300 ${
+          warning
+            ? "bg-rose-100 text-rose-900 border-l-4 border-rose-900"
+            : "bg-teal-100 text-teal-900 border-l-4 border-teal-900"
+        }`}
+      >
+        <div className="flex items-center">
+          <div className="mr-3">
+            {warning ? (
+              <ExclamationTriangleIcon className="w-6 h-6 text-rose-900" />
+            ) : (
+              <CheckCircleIcon className="w-6 h-6 text-teal-900" />
+            )}
+          </div>
+          <div>
+            <h2 className="text-lg font-semibold">{message}</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default CalloutCard;
+  
